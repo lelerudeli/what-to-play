a347ad81cc76784b5f13969ab4d612c648463c50
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-11-27 01:08:42 -0300

    Ajustando erros do merge anterior

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,20 +1,41 @@
 from flask import Flask, render_template
+from app.dao.avalia_dao import lista_avaliacoes, listar_avaliacoes
+from app.dao.classificação_etaria_dao import lista_classificacoes_etarias, listar_classificacoes_etarias
+from app.dao.favorita_dao import lista_favoritos, listar_favoritos
+from app.dao.jogos_dao import lista_jogos, listar_jogos
 from app.dao.usuario_dao import lista_usuarios, listar_usuarios
 from config import Config 
+from flask_cors import CORS
 
 def iniciar_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app)
     
     @app.route('/')
     def usuarios():
-        print("Rota /usuarios chamada!")  
+        """Rota principal que exibe todos os dados em uma única página"""
+        # Obter os dados de cada tabela
         usuarios = listar_usuarios(app)
+        jogos = listar_jogos(app)
+        avaliacoes = listar_avaliacoes(app)
+        favoritos = listar_favoritos(app)
+        classificacoes_etarias = listar_classificacoes_etarias(app)
+
+        # Gerar os HTMLs
         html_usuarios = lista_usuarios(usuarios)
-
-        return render_template('user.html', usuarios_html=html_usuarios)
+        html_jogos = lista_jogos(jogos)
+        html_avaliacoes = lista_avaliacoes(avaliacoes)
+        html_favoritos = lista_favoritos(favoritos)
+        html_classificacoes_etarias = lista_classificacoes_etarias(classificacoes_etarias)
         
-    return app
-    
-    
+        return render_template(
+            'index.html',
+            usuarios_html=html_usuarios,
+            jogos_html=html_jogos,
+            avaliacoes_html=html_avaliacoes,
+            favoritos_html=html_favoritos,
+            classificacoes_etarias_html=html_classificacoes_etarias
+        )
 
+    return app
---
+++ backend/app/dao/jogos_dao.py
@@ -0,0 +1,32 @@
+from app.conexao_banco import conexao_abrir, conexao_fechar
+
+def listar_jogos(app):
+    """ Lista todos os jogos do banco de dados. """
+    config = app.config
+    con = conexao_abrir(config)
+    
+    with con.cursor(dictionary=True) as cursor:
+        cursor.execute("SELECT * FROM Jogo")
+        jogos = cursor.fetchall()
+    
+    conexao_fechar(con)  # Fecha a conexão
+    return jogos
+    
+# def lista_jogos(jogos):
+#     """Exibe a lista de jogos do banco de dados como string"""
+    
+#     html_jogos = ""  
+#     for jogo in jogos:
+#         html_jogos += (
+#             f"ID Jogo: {jogo['idJogo']} <br> "
+#             f"Nome do Jogo: {jogo['nomeJogo']} <br> "
+#             f"Tipo de Jogo: {jogo['tipoJogo']} <br> "
+#             f"Faixa Etária: {jogo['faixaEtaria']} <br> "
+#             f"Número de Curtidas: {jogo['numCurtidas']} <br> "
+#             f"Número de Jogadores: {jogo['numJogadores']} <br> "
+#             f"Data de Lançamento: {jogo['dataLancamento']} <br> "
+#             f"Gênero: {jogo['generoJogo']} <br> "
+#             f"Plataforma: {jogo['plataformaJogo']} <br><br>"
+#         )
+        
+#     return html_jogos
