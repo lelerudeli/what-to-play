3f999404f554b1348d8f3e30d2b55f230bc295da
Rodrigo Bettio <rodriguinhobettiojr1@gmail.com>
2024-12-08 20:28:25 -0300

    Atualizando as rotas

--- backend/app.py
+++ backend/app.py
@@ -78,6 +78,93 @@ def get_favoritos():
 def get_classificacoes_etarias():
     return jsonify(obter_classificacoes_etarias(con))
 
+# Página de cadastro
+    @app.route('/cadastro', methods=['POST'])
+    def cadastro():
+        """Página inicial: cadastro de usuário"""
+        usuario_infos = request.json
+        novo_id = criar_usuario(app, usuario_infos)
+        return jsonify({'id': novo_id}), 201
+    
+    # Página de login
+    @app.route('/login', methods=['POST'])
+    def login():
+        """Endpoint para autenticação de usuário."""
+        login_infos = request.json
+        id_usuario = autenticar_usuario(app, login_infos)
+
+        if id_usuario:
+            return jsonify({"mensagem": "Login realizado com sucesso!", "id": id_usuario}), 200
+        else:
+            return jsonify({"erro": "Credenciais inválidas!"}), 401
+        
+    @app.route('/esqueci-senha', methods=['POST'])
+    def esqueci_senha():
+        """Verifica se o e-mail existe no sistema para redefinição de senha."""
+        email = request.json
+        
+        if not email:
+            return jsonify({"erro": "E-mail não fornecido."}), 400
+        
+        # Verificar se o e-mail existe no banco de dados
+        usuario = verificar_email(app, email)
+
+        if usuario:
+            return jsonify({
+                "mensagem": "E-mail encontrado.",
+                "email": usuario['emailUsuario'],  # Retorna o e-mail para o próximo passo
+                "id": usuario['idUsuario']        # Retorna o ID para identificar o usuário
+            }), 200
+        else:
+            return jsonify({"erro": "E-mail não encontrado."}), 404
+        
+    @app.route('/jogos', methods=['GET'])
+    def obter_todos_jogos():
+        """Obter todos os jogos."""
+        jogos = obter_jogos(app)
+        
+        if jogos:
+            return jsonify(jogos)
+        return jsonify({'error': 'Nenhum jogo encontrado'}), 404
+
+    @app.route('/jogos/<string:tipo>', methods=['GET'])
+    def obter_jogos_por_tipo(tipo):
+        """Obter jogos de um tipo específico."""
+        jogos = obter_jogo_por_tipo(app, tipo)
+        
+        if jogos:
+            return jsonify(jogos)
+        return jsonify({'error': 'Nenhum jogo encontrado para o tipo especificado'}), 404
+    
+    @app.route('/jogos/<int:id>', methods=['GET'])
+    def obter_jogo(id):
+        """Obter informações de um jogo específico pelo ID."""
+        jogo = obter_jogo_por_id(app, id)
+        
+        if jogo:
+            return jsonify(jogo)
+        return jsonify({'error': 'Jogo não encontrado'}), 404
+
+    @app.route('/jogos/<int:id>', methods=['PUT'])
+    def atualizar_jogo_por_id(id):
+        """Atualizar informações de um jogo específico pelo ID."""
+        jogo_infos = request.json
+
+        linhas_atualizadas = atualizar_jogo(app, id, jogo_infos)
+        if linhas_atualizadas > 0:
+            return jsonify({'message': 'Jogo atualizado com sucesso'}), 200
+        return jsonify({'error': 'Jogo não encontrado ou sem alterações'}), 404
+  
+    @app.route('/jogos/<int:id>', methods=['DELETE'])
+    def excluir_jogo_por_id(id):
+        """Excluir um jogo específico pelo ID."""
+        linhas_afetadas = excluir_jogo(app, id)
+        if linhas_afetadas > 0:
+            return jsonify({'message': 'Jogo excluído com sucesso'}), 200
+        return jsonify({'error': 'Jogo não encontrado'}), 404
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 # Definição de rotas com post ou get
